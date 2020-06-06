b81cd58c518d088e632091c19d66f606b4161571
Claycorp <Claycorp@doubledoordev.net>
2020-06-06 17:31:10 -0500

    99% done port to 1.14 Still can't get keys to work regardless of anything tried.

--- src/main/java/net/dries007/tapemouse/CommandTapeMouse.java
+++ src/main/java/net/dries007/tapemouse/CommandTapeMouse.java
@@ -1,142 +1,142 @@
-package net.dries007.tapemouse;
-
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.settings.KeyBinding;
-import net.minecraft.command.CommandBase;
-import net.minecraft.command.CommandException;
-import net.minecraft.command.ICommandSender;
-import net.minecraft.command.NumberInvalidException;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.text.Style;
-import net.minecraft.util.text.TextComponentString;
-import net.minecraft.util.text.TextFormatting;
-import net.minecraftforge.fml.relauncher.ReflectionHelper;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
-
-import javax.annotation.Nullable;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-import static org.lwjgl.input.Keyboard.KEY_NONE;
-
-/**
- * @author Dries007
- */
-@SideOnly(Side.CLIENT)
-public class CommandTapeMouse extends CommandBase
-{
-    private static final Map<String, KeyBinding> KEYBIND_ARRAY = ReflectionHelper.getPrivateValue(KeyBinding.class, null, "KEYBIND_ARRAY", "field_74516_a");
-    private boolean prevPauseSetting = true; // defaults to true
-
-    @Override
-    public String getName()
-    {
-        return TapeMouse.MODID.toLowerCase();
-    }
-
-    @Override
-    public boolean checkPermission(MinecraftServer server, ICommandSender sender)
-    {
-        return sender instanceof EntityPlayer;
-    }
-
-    @Override
-    public String getUsage(ICommandSender sender)
-    {
-        return '/' + getName() + " [off|keybinding name ...] [delay] OR Use no arguments to get a list of keybindings.";
-    }
-
-    @Override
-    public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException
-    {
-        if (args.length == 0)
-        {
-            sender.sendMessage(new TextComponentString("List of keybindings").setStyle(new Style().setColor(TextFormatting.AQUA)));
-            sender.sendMessage(new TextComponentString("Key => NAME (category)").setStyle(new Style().setColor(TextFormatting.AQUA)));
-
-            for (KeyBinding keyBinding : KEYBIND_ARRAY.values())
-            {
-                if (keyBinding == null || keyBinding.getKeyCode() == KEY_NONE) continue;
-                String name = keyBinding.getKeyDescription();
-                if (name == null) continue;
-                name = name.replaceFirst("^key\\.", "");
-
-                String cat = keyBinding.getKeyCategory();
-                if (cat == null) continue;
-                cat = cat.replaceFirst("^key\\.", "");
-                cat = cat.replaceFirst("^categories\\.", "");
-
-                sender.sendMessage(new TextComponentString(keyBinding.getDisplayName() + " => " + name + " (" + cat + ")"));
-            }
-            return;
-        }
-        if (args[0].equalsIgnoreCase("off"))
-        {
-            Minecraft.getMinecraft().gameSettings.pauseOnLostFocus = prevPauseSetting;
-            TapeMouse.keyBinding = null;
-            TapeMouse.i = 0;
-            sender.sendMessage(new TextComponentString("TapeMouse off."));
-        }
-        else
-        {
-            StringBuilder askedName = new StringBuilder(args[0]);
-            if (args.length > 1)
-            {
-                try
-                {
-                    TapeMouse.delay = parseInt(args[args.length - 1], 0);
-                    for (int i = 1; i < args.length - 1; i++) askedName.append(' ').append(args[i]);
-                }
-                catch (NumberInvalidException e)
-                {
-                    // Assume last part was not a number
-                    for (int i = 1; i < args.length; i++) askedName.append(' ').append(args[i]);
-                }
-                askedName = new StringBuilder(askedName.toString().trim());
-            }
-            for (KeyBinding keyBinding : KEYBIND_ARRAY.values())
-            {
-                if (keyBinding == null) continue;
-                String name = keyBinding.getKeyDescription();
-                //noinspection ConstantConditions Because of stupid mod authors...
-                if (name == null) continue;
-                name = name.replaceFirst("^key\\.", "");
-                if (askedName.toString().equalsIgnoreCase(name))
-                {
-                    prevPauseSetting = Minecraft.getMinecraft().gameSettings.pauseOnLostFocus;
-                    Minecraft.getMinecraft().gameSettings.pauseOnLostFocus = false;
-                    TapeMouse.keyBinding = keyBinding;
-                    sender.sendMessage(new TextComponentString("TapeMouse on '" + name + "' (" + keyBinding.getDisplayName() + ") with delay " + TapeMouse.delay + " ticks."));
-                    return;
-                }
-            }
-
-            throw new CommandException(askedName + " is unknown keybinding. If your keybind ends in a number, you *have* to specify the delay.");
-        }
-    }
-
-    @Override
-    public List<String> getTabCompletions(MinecraftServer server, ICommandSender sender, String[] args, @Nullable BlockPos pos)
-    {
-        if (args.length == 1)
-        {
-            List<String> list = new ArrayList<>();
-            list.add("off");
-            for (KeyBinding keyBinding : KEYBIND_ARRAY.values())
-            {
-                if (keyBinding == null || keyBinding.getKeyCode() == KEY_NONE) continue;
-                String name = keyBinding.getKeyDescription();
-                //noinspection ConstantConditions Because of stupid mod authors...
-                if (name == null) continue;
-                name = name.replaceFirst("^key\\.", "");
-                list.add(name);
-            }
-            return getListOfStringsMatchingLastWord(args, list);
-        }
-        return super.getTabCompletions(server, sender, args, pos);
-    }
-}
+//package net.dries007.tapemouse;
+//
+//import net.minecraft.client.Minecraft;
+//import net.minecraft.client.settings.KeyBinding;
+//import net.minecraft.command.CommandBase;
+//import net.minecraft.command.CommandException;
+//import net.minecraft.command.ICommandSender;
+//import net.minecraft.command.NumberInvalidException;
+//import net.minecraft.entity.player.EntityPlayer;
+//import net.minecraft.server.MinecraftServer;
+//import net.minecraft.util.math.BlockPos;
+//import net.minecraft.util.text.Style;
+//import net.minecraft.util.text.TextComponentString;
+//import net.minecraft.util.text.TextFormatting;
+//import net.minecraftforge.fml.relauncher.ReflectionHelper;
+//import net.minecraftforge.fml.relauncher.Side;
+//import net.minecraftforge.fml.relauncher.SideOnly;
+//
+//import javax.annotation.Nullable;
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Map;
+//
+//import static org.lwjgl.input.Keyboard.KEY_NONE;
+//
+///**
+// * @author Dries007
+// */
+//@SideOnly(Side.CLIENT)
+//public class CommandTapeMouse extends CommandBase
+//{
+//    private static final Map<String, KeyBinding> KEYBIND_ARRAY = ReflectionHelper.getPrivateValue(KeyBinding.class, null, "KEYBIND_ARRAY", "field_74516_a");
+//    private boolean prevPauseSetting = true; // defaults to true
+//
+//    @Override
+//    public String getName()
+//    {
+//        return TapeMouse.MODID.toLowerCase();
+//    }
+//
+//    @Override
+//    public boolean checkPermission(MinecraftServer server, ICommandSender sender)
+//    {
+//        return sender instanceof EntityPlayer;
+//    }
+//
+//    @Override
+//    public String getUsage(ICommandSender sender)
+//    {
+//        return '/' + getName() + " [off|keybinding name ...] [delay] OR Use no arguments to get a list of keybindings.";
+//    }
+//
+//    @Override
+//    public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException
+//    {
+//        if (args.length == 0)
+//        {
+//            sender.sendMessage(new TextComponentString("List of keybindings").setStyle(new Style().setColor(TextFormatting.AQUA)));
+//            sender.sendMessage(new TextComponentString("Key => NAME (category)").setStyle(new Style().setColor(TextFormatting.AQUA)));
+//
+//            for (KeyBinding keyBinding : KEYBIND_ARRAY.values())
+//            {
+//                if (keyBinding == null || keyBinding.getKeyCode() == KEY_NONE) continue;
+//                String name = keyBinding.getKeyDescription();
+//                if (name == null) continue;
+//                name = name.replaceFirst("^key\\.", "");
+//
+//                String cat = keyBinding.getKeyCategory();
+//                if (cat == null) continue;
+//                cat = cat.replaceFirst("^key\\.", "");
+//                cat = cat.replaceFirst("^categories\\.", "");
+//
+//                sender.sendMessage(new TextComponentString(keyBinding.getDisplayName() + " => " + name + " (" + cat + ")"));
+//            }
+//            return;
+//        }
+//        if (args[0].equalsIgnoreCase("off"))
+//        {
+//            Minecraft.getMinecraft().gameSettings.pauseOnLostFocus = prevPauseSetting;
+//            TapeMouse.keyBinding = null;
+//            TapeMouse.i = 0;
+//            sender.sendMessage(new TextComponentString("TapeMouse off."));
+//        }
+//        else
+//        {
+//            StringBuilder askedName = new StringBuilder(args[0]);
+//            if (args.length > 1)
+//            {
+//                try
+//                {
+//                    TapeMouse.delay = parseInt(args[args.length - 1], 0);
+//                    for (int i = 1; i < args.length - 1; i++) askedName.append(' ').append(args[i]);
+//                }
+//                catch (NumberInvalidException e)
+//                {
+//                    // Assume last part was not a number
+//                    for (int i = 1; i < args.length; i++) askedName.append(' ').append(args[i]);
+//                }
+//                askedName = new StringBuilder(askedName.toString().trim());
+//            }
+//            for (KeyBinding keyBinding : KEYBIND_ARRAY.values())
+//            {
+//                if (keyBinding == null) continue;
+//                String name = keyBinding.getKeyDescription();
+//                //noinspection ConstantConditions Because of stupid mod authors...
+//                if (name == null) continue;
+//                name = name.replaceFirst("^key\\.", "");
+//                if (askedName.toString().equalsIgnoreCase(name))
+//                {
+//                    prevPauseSetting = Minecraft.getMinecraft().gameSettings.pauseOnLostFocus;
+//                    Minecraft.getMinecraft().gameSettings.pauseOnLostFocus = false;
+//                    TapeMouse.keyBinding = keyBinding;
+//                    sender.sendMessage(new TextComponentString("TapeMouse on '" + name + "' (" + keyBinding.getDisplayName() + ") with delay " + TapeMouse.delay + " ticks."));
+//                    return;
+//                }
+//            }
+//
+//            throw new CommandException(askedName + " is unknown keybinding. If your keybind ends in a number, you *have* to specify the delay.");
+//        }
+//    }
+//
+//    @Override
+//    public List<String> getTabCompletions(MinecraftServer server, ICommandSender sender, String[] args, @Nullable BlockPos pos)
+//    {
+//        if (args.length == 1)
+//        {
+//            List<String> list = new ArrayList<>();
+//            list.add("off");
+//            for (KeyBinding keyBinding : KEYBIND_ARRAY.values())
+//            {
+//                if (keyBinding == null || keyBinding.getKeyCode() == KEY_NONE) continue;
+//                String name = keyBinding.getKeyDescription();
+//                //noinspection ConstantConditions Because of stupid mod authors...
+//                if (name == null) continue;
+//                name = name.replaceFirst("^key\\.", "");
+//                list.add(name);
+//            }
+//            return getListOfStringsMatchingLastWord(args, list);
+//        }
+//        return super.getTabCompletions(server, sender, args, pos);
+//    }
+//}
--- src/main/java/net/dries007/tapemouse/TapeMouse.java
+++ src/main/java/net/dries007/tapemouse/TapeMouse.java
@@ -1,69 +1,173 @@
 package net.dries007.tapemouse;
 
+import java.util.Map;
+
+import org.apache.commons.lang3.tuple.Pair;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiChat;
-import net.minecraft.client.gui.GuiMainMenu;
+import net.minecraft.client.gui.NewChatGui;
+import net.minecraft.client.gui.screen.ChatScreen;
+import net.minecraft.client.gui.screen.MainMenuScreen;
 import net.minecraft.client.settings.KeyBinding;
-import net.minecraftforge.client.ClientCommandHandler;
+import net.minecraft.client.util.InputMappings;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.Style;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.ClientChatEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.EventPriority;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent;
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
+import net.minecraftforge.fml.loading.FMLEnvironment;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
 
 /**
  * Main mod file
  *
- * @author Dries007
+ * @author Dries007. Claycorp
  */
-@SuppressWarnings({"NewExpressionSideOnly", "MethodCallSideOnly"}) // @Mod.clientSideOnly does this.
-@Mod(modid = TapeMouse.MODID, name = TapeMouse.NAME, dependencies = "before:*", useMetadata = false, clientSideOnly = true)
+@Mod("tapemouse")
 public class TapeMouse
 {
-    public static final String MODID = "tapemouse";
     public static final String NAME = "TapeMouse";
     static int delay = 10;
-    static KeyBinding keyBinding;
+    private static final Map<String, KeyBinding> KEYBIND_ARRAY = ObfuscationReflectionHelper.getPrivateValue(KeyBinding.class, null, "field_74516_a");
     static int i = 0;
+    static KeyBinding keyToPress;
 
-    @Mod.EventHandler
-    public void init(FMLPreInitializationEvent event)
+    public TapeMouse()
     {
-        MinecraftForge.EVENT_BUS.register(this);
-        if (event.getSide().isClient()) ClientCommandHandler.instance.registerCommand(new CommandTapeMouse());
+        //Because forge is a bunch of dunces and make devs do all this here instead of just marking the side in the mods.toml file... We get all this great shit.
+        //can't wait to see how may devs fuck this all up... :rolling_eyes:
+        if (FMLEnvironment.dist == Dist.CLIENT)
+            MinecraftForge.EVENT_BUS.register(this);
+        //To stop the server reporting this mod is missing because forge is fucking stupid.
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+    }
+
+    //This whole thing is a total hack till client commands are a thing (if that's ever)
+    @SubscribeEvent
+    public void onChat(ClientChatEvent event)
+    {
+        String originalMessage = event.getOriginalMessage();
+        NewChatGui clientChat = Minecraft.getInstance().ingameGUI.getChatGUI();
+        boolean prevPauseSetting = true;
+
+        if (originalMessage.contains("/tapemouse"))
+        {
+            String[] args = originalMessage.split("\\s", 3);
+
+            switch (args.length)
+            {
+                case 1:
+                {
+                    clientChat.printChatMessage(new TranslationTextComponent("tapemouse.command.keybindlist").setStyle(new Style().setColor(TextFormatting.AQUA)));
+                    clientChat.printChatMessage(new TranslationTextComponent("tapemouse.command.keybindlistinfo").setStyle(new Style().setColor(TextFormatting.AQUA)));
+
+                    for (KeyBinding keyBinding : KEYBIND_ARRAY.values())
+                    {
+                        if (keyBinding == null || keyBinding.isInvalid()) continue;
+                        String name = keyBinding.getKeyDescription();
+                        name = name.replaceFirst("^key\\.", "");
+
+                        String cat = keyBinding.getKeyCategory();
+                        cat = cat.replaceFirst("^key\\.", "");
+                        cat = cat.replaceFirst("^categories\\.", "");
+
+                        clientChat.printChatMessage(new StringTextComponent(keyBinding.getLocalizedName() + " => " + name + " (" + cat + ")"));
+                    }
+                    break;
+                }
+                case 2:
+                {
+                    if (args[1].equals("off"))
+                    {
+                        Minecraft.getInstance().gameSettings.pauseOnLostFocus = prevPauseSetting;
+                        keyToPress = null;
+                        i = 0;
+                        clientChat.printChatMessage(new TranslationTextComponent("tapemouse.command.off")
+                                .setStyle(new Style().setBold(true).setColor(TextFormatting.RED)));
+                    }
+                    else
+                    {
+                        clientChat.printChatMessage(new TranslationTextComponent("tapemouse.command.missingdelay")
+                                .setStyle(new Style().setBold(true).setColor(TextFormatting.RED)));
+                    }
+                    break;
+                }
+                case 3:
+
+                    delay = Integer.parseInt(args[2]);
+
+                    for (KeyBinding keyBinding : KEYBIND_ARRAY.values())
+                    {
+                        if (keyBinding == null) continue;
+                        String name = keyBinding.getKeyDescription();
+                        name = name.replaceFirst("^key\\.", "");
+                        if (args[1].equalsIgnoreCase(name))
+                        {
+                            //prevPauseSetting = Minecraft.getInstance().gameSettings.pauseOnLostFocus;
+                            Minecraft.getInstance().gameSettings.pauseOnLostFocus = false;
+                            keyToPress = keyBinding;
+
+                            clientChat.printChatMessage(new TranslationTextComponent("tapemouse.command.set", name, keyBinding.getLocalizedName(), TapeMouse.delay)
+                                    .setStyle(new Style().setBold(true).setColor(TextFormatting.GREEN)));
+                            return;
+                        }
+                    }
+            }
+
+            clientChat.addToSentMessages(originalMessage);
+            event.setCanceled(true);
+        }
     }
 
     @SubscribeEvent(priority = EventPriority.HIGHEST)
     public void textRenderEvent(RenderGameOverlayEvent.Text event)
     {
-        if (keyBinding == null) return;
-        if (Minecraft.getMinecraft().currentScreen instanceof GuiMainMenu)
+        if (keyToPress == null) return;
+        if (Minecraft.getInstance().currentScreen instanceof MainMenuScreen)
         {
-            event.getLeft().add(NAME + " paused. Main menu open. If you want to AFK, use ALT+TAB.");
+            event.getLeft().add(new TranslationTextComponent("tapemouse.paused.mainmenu")
+                    .setStyle(new Style().setBold(true).setColor(TextFormatting.RED)).getFormattedText());
         }
-        else if (Minecraft.getMinecraft().currentScreen instanceof GuiChat)
+        else if (Minecraft.getInstance().currentScreen instanceof ChatScreen)
         {
-            event.getLeft().add(NAME + " paused. Chat GUI open. If you want to AFK, use ALT+TAB.");
+            event.getLeft().add(new TranslationTextComponent("tapemouse.paused.chat")
+                    .setStyle(new Style().setBold(true).setColor(TextFormatting.RED)).getFormattedText());
         }
         else
         {
-            event.getLeft().add(NAME + " active: " + keyBinding.getDisplayName() + " (" + keyBinding.getKeyDescription().replaceFirst("^key\\.", "") + ')');
-            event.getLeft().add("Delay: " + i + " / " + delay);
+            event.getLeft().add(new TranslationTextComponent("tapemouse.active.info", keyToPress.getLocalizedName(), keyToPress.getKeyDescription().replaceFirst("^key\\.", ""))
+                    .setStyle(new Style().setBold(true).setColor(TextFormatting.GOLD)).getFormattedText());
+
+            event.getLeft().add(new TranslationTextComponent("tapemouse.active.delay", i, delay)
+                    .setStyle(new Style().setBold(true).setColor(TextFormatting.GOLD)).getFormattedText());
         }
     }
 
     @SubscribeEvent(priority = EventPriority.HIGHEST)
     public void tickEvent(TickEvent.ClientTickEvent event)
     {
-        if (event.phase != TickEvent.Phase.START) return;
-        if (Minecraft.getMinecraft().currentScreen instanceof GuiMainMenu) return;
-        if (Minecraft.getMinecraft().currentScreen instanceof GuiChat) return;
-        if (keyBinding == null) return;
+        if (event.phase != TickEvent.Phase.START && !event.side.isServer()) return;
+        if (Minecraft.getInstance().currentScreen instanceof MainMenuScreen) return;
+        if (Minecraft.getInstance().currentScreen instanceof ChatScreen) return;
+        if (keyToPress == null) return;
         if (i++ < delay) return;
         i = 0;
-        if (delay == 0) KeyBinding.setKeyBindState(keyBinding.getKeyCode(), true);
-        KeyBinding.onTick(keyBinding.getKeyCode());
+        if (delay == 0) KeyBinding.setKeyBindState(keyToPress.getKey(), true);
+        //KeyBinding.onTick(keyToPress.getKey());
+        if (keyToPress.getKey().getType() == InputMappings.Type.KEYSYM)
+        {
+            KeyBinding.setKeyBindState(keyToPress.getKey(), true);
+            KeyBinding.onTick(keyToPress.getKey());
+            KeyBinding.setKeyBindState(keyToPress.getKey(), false);
+        }
     }
 }
